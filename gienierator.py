2955f1e2ba64a43750b54937482ef17762b46212
Maciej Bielecki <zyla@prati.pl>
2016-12-21 14:34:18 +0100

    Changes so far

--- gienierator.py
+++ gienierator.py
@@ -3,3 +3,15 @@
 print("# *Zadanie 2*: obsługa konferencji naukowej")
 print("- zgłaszanie referatów, recenzowanie")
 print("- organizacja eventu - program wystąpień, eventy typu przyjęcie, wycieczka ze zwiedzaniem itp.")
+
+print("## Przypadki Biznesowe")
+
+state = object()
+
+def reset():
+    state.kolejny_punkt = 1
+
+def przypadek(id, nazwa):
+    reset()
+
+    print("### %s %s" % (id, nazwa))
