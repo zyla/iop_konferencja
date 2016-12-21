1917ea076bc02ad9ff444f15fa8a99ee749332b9
Maciej Bielecki <zyla@prati.pl>
2016-12-21 14:37:58 +0100

    Changes so far

--- gienierator.py
+++ gienierator.py
@@ -7,6 +7,8 @@ print("- organizacja eventu - program wystąpień, eventy typu przyjęcie, wycie
 print("## Przypadki Biznesowe")
 
 state = object()
+state.kolejny_pb = 1
+state.kolejny_fu = 1
 
 def reset():
     state.kolejny_punkt = 1
@@ -15,3 +17,20 @@ def przypadek(id, nazwa):
     reset()
 
     print("### %s %s" % (id, nazwa))
+
+def PB(nazwa):
+    przypadek('PB' + state.kolejny_pb, nazwa)
+    state.kolejny_pb += 1
+
+def punkt(tresc):
+    print('%d. %s' % (state.kolejny_punkt, tresc))
+    state.kolejny_punkt += 1
+
+PB('Zgłoszenie referatu')
+punkt("""Prelegent wypełnia formularz z danymi:
+ - temat prezentacji
+ - abstrakt
+ - dane kontaktowe
+ - draft publikacji""")
+punkt("System zapisuje publikację")
+punkt("System wysyła e-mail z potwierdzeniem do prelegenta")
