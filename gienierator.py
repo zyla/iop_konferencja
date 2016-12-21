e0905cb12ca7baaea006f8ef0117698158962888
Maciej Bielecki <zyla@prati.pl>
2016-12-21 14:31:48 +0100

    Changes so far

---
+++ gienierator.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+print("# *Zadanie 2*: obsługa konferencji naukowej")
+print("- zgłaszanie referatów, recenzowanie")
+print("- organizacja eventu - program wystąpień, eventy typu przyjęcie, wycieczka ze zwiedzaniem itp.")
